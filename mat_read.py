1c62b1fa5f71997c0a8b839f0c4d6f7638a283e1
virajk <virajk@saksglobal.com>
2021-08-05 05:39:32 -0400

    save annotations for csv

---
+++ mat_read.py
@@ -0,0 +1,25 @@
+import scipy.io
+import csv
+import glob
+import os
+
+mat = scipy.io.loadmat('data/source/airplanes_anno/annotation_0001.mat')
+
+# print(mat)
+# print(mat['box_coord'])
+# print(mat['box_coord'][0][0])
+# print(mat['box_coord'][0][1])
+# print(mat['box_coord'][0][2])
+# print(mat['box_coord'][0][3])
+
+f = csv.writer(open('data/annotation.csv', "w", newline=''))
+f.writerow(['image_name', 'x1', 'y1', 'x2', 'y2'])
+
+for file in glob.glob('data/source/airplanes_anno/*.mat'):
+
+    file_name = os.path.splitext(os.path.basename(file))[0]
+
+    image_name = 'image_' + file_name.split('_')[1] + '.jpg'
+
+    mat = scipy.io.loadmat(file)
+    f.writerow([image_name, mat['box_coord'][0][0], mat['box_coord'][0][1], mat['box_coord'][0][2], mat['box_coord'][0][3]])
